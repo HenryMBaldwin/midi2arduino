4ff175ce2315ea71506a4472386c58abd38031e0
Henry Baldwin <henrymbaldwin@gmail.com>
2024-06-22 22:06:41 -0400

    Modified file to work as vercel serverless function.

--- midi2arduino.py
+++ midi2arduino.py
@@ -1,6 +1,12 @@
 import argparse
 import mido
 from mido import MidiFile
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+import os
+
+app = FastAPI()
 
 midiDic = []
 notesOn = []
@@ -11,78 +17,96 @@ tempo = None
 
 for midinum in range(128):
     midiDic.append(0)
-    freq = 440 * float(2 ** float(float(midinum-69) / 12))
+    freq = 440 * float(2 ** float(float(midinum - 69) / 12))
     midiDic[midinum] = freq
 
 def midiNUM(num):
     return midiDic[num]
 
-midiFile = MidiFile("/Users/henry/Dev/PycharmProjects/miditoarduino/midis/Dragonborn - Skyrim [MIDICollection.net].mid")
-mergedMidiFile = MidiFile()
-mergedMidiFile.tracks.append(mido.merge_tracks(midiFile.tracks))
+@app.post("/api/convert")
+async def convert(file: UploadFile = File(...)):
+    global notesOn, lastTime, highestFileNumber, totalTime, tempo
+    notesOn = []
+    lastTime = 0
+    highestFileNumber = 0
+    totalTime = 0
+    tempo = None
 
+    contents = await file.read()
+    midiFile = MidiFile(BytesIO(contents))
+    mergedMidiFile = MidiFile()
+    mergedMidiFile.tracks.append(mido.merge_tracks(midiFile.tracks))
 
-for msg in mergedMidiFile:
-    print msg
+    output_files = {}
+    for msg in mergedMidiFile:
+        msgDict = msg.dict()
 
-    msgDict = msg.dict()
+        messageAdded = False
+        noteIndex = -1
+        totalTime += msg.time
 
-    messageAdded = False
-    noteIndex = -1
-    totalTime += msg.time
+        if msg.is_meta:
+            metaDic = msg.dict()
+            if 'tempo' in metaDic:
+                tempo = metaDic['tempo']
 
+        if msg.time != 0:
+            for i in range(((len(notesOn) - 1) / 2) + 1):
+                if i not in output_files:
+                    output_files[i] = BytesIO()
 
-    if msg.is_meta:
-        metaDic = msg.dict()
-        if 'tempo' in metaDic:
-            tempo = metaDic['tempo']
+                output_files[i].write('  delay({});\n'.format((msg.time / 1.5) * 1000).encode('utf-8'))
 
-    if msg.time != 0:
-        for i in range(((len(notesOn) - 1) / 2) + 1):
-            arduinoFile = open('/Users/henry/Dev/PycharmProjects/miditoarduino/Output/Arduino{}.txt'.format(i), 'a+')
-
-            arduinoFile.write('  delay({});\n'.format((msg.time / 1.5) * 1000))
-            arduinoFile.close()
-
-    if 'note' in msgDict:
-        if msg.type == 'note_on' and msg.velocity != 0:
-            for index, io in enumerate(notesOn):
-                if type(io) == int:
-                    notesOn[index] = msg
-                    noteIndex = index
-                    messageAdded = True
-                    break
-
-            if not messageAdded:
-                notesOn.append(msg)
-                noteIndex = len(notesOn) - 1
-
-            fileNumber = noteIndex / 2
-            toneNumber = noteIndex % 2
-
-            arduinoFile = open('/Users/henry/Dev/PycharmProjects/miditoarduino/Output/Arduino{}.txt'.format(fileNumber), 'a+')
-
-            if fileNumber > highestFileNumber and totalTime != 0:
-                highestFileNumber = fileNumber
-                arduinoFile.write('  delay({});\n'.format((totalTime / 1.5) * 1000))
-
-            arduinoFile.write('  tone{}.play({});\n'.format(toneNumber, midiNUM(msg.note)))
-            arduinoFile.close()
-
-        elif msg.type == 'note_off' or msg.velocity == 0:
-            for index, item in enumerate(notesOn):
-                if not type(item) == int:
-                    if msg.note == item.note and msg.channel == item.channel:
-                        notesOn[index] = 0
+        if 'note' in msgDict:
+            if msg.type == 'note_on' and msg.velocity != 0:
+                for index, io in enumerate(notesOn):
+                    if type(io) == int:
+                        notesOn[index] = msg
                         noteIndex = index
+                        messageAdded = True
                         break
 
-            fileNumber = noteIndex / 2
-            toneNumber = noteIndex % 2
+                if not messageAdded:
+                    notesOn.append(msg)
+                    noteIndex = len(notesOn) - 1
 
-            arduinoFile = open('/Users/henry/Dev/PycharmProjects/miditoarduino/Output/Arduino{}.txt'.format(fileNumber),'a+')
+                fileNumber = noteIndex // 2
+                toneNumber = noteIndex % 2
 
-            arduinoFile.write('  tone{}.stop();\n'.format(toneNumber))
-            arduinoFile.close()
+                if fileNumber not in output_files:
+                    output_files[fileNumber] = BytesIO()
 
+                if fileNumber > highestFileNumber and totalTime != 0:
+                    highestFileNumber = fileNumber
+                    output_files[fileNumber].write('  delay({});\n'.format((totalTime / 1.5) * 1000).encode('utf-8'))
 
+                output_files[fileNumber].write('  tone{}.play({});\n'.format(toneNumber, midiNUM(msg.note)).encode('utf-8'))
+
+            elif msg.type == 'note_off' or msg.velocity == 0:
+                for index, item in enumerate(notesOn):
+                    if not type(item) == int:
+                        if msg.note == item.note and msg.channel == item.channel:
+                            notesOn[index] = 0
+                            noteIndex = index
+                            break
+
+                fileNumber = noteIndex // 2
+                toneNumber = noteIndex % 2
+
+                if fileNumber not in output_files:
+                    output_files[fileNumber] = BytesIO()
+
+                output_files[fileNumber].write('  tone{}.stop();\n'.format(toneNumber).encode('utf-8'))
+
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for i, content in output_files.items():
+            content.seek(0)
+            zip_file.writestr(f'Arduino{i}.txt', content.read())
+
+    zip_buffer.seek(0)
+    return StreamingResponse(zip_buffer, media_type='application/zip', headers={"Content-Disposition": "attachment; filename=output.zip"})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
