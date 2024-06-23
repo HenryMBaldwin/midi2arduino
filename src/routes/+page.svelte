baa2575d00912b9fb1407b5a7dd903c71a6695be
Henry Baldwin <henrymbaldwin@gmail.com>
2024-06-23 11:37:09 -0400

    Changed wording and formatting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import LoaderButton from "$lib/components/LoaderButton.svelte";
 	import { FileDropzone } from "@skeletonlabs/skeleton";
-	import { onMount } from "svelte";
 
 	let fileLoading = false;
 	let uploadedFile: File | null = null;
@@ -52,7 +51,9 @@
 	}
 </script>
 
-<p>Welcome! MIDI2Arduino is a tool to convert MIDI files to be played on one of the most accessible Arduino platforms, the Arduino UNO. Specifically, this transpiler converts midi to arduino code using the the Arduino Tone library. Due to the number of hardware timers on the Arduino UNO, only two tones can be generated on one Arduino UNO at a time. This means multiple files and Arduinos may be necessary to play a full MIDI file depending on the song. This transpiler will output the minimum number of code files in a .zip necessary to represent the given mid . To sync the start of multiple Arduinos playback, it is recommended that a single button is wired to all Arduinos and programmed to start playback. Because of this, the outputted files will be txt files so the code can be easily copied rather than Arduino code files.</p>
+<p>Welcome! MIDI2Arduino is a tool to convert MIDI files to be played on one of the most accessible Arduino platforms, the Arduino UNO. </p>
+<p>This transpiler converts midi to arduino code using the the Arduino Tone library. Due to the number of hardware timers on the Arduino UNO, only two tones can be generated on one Arduino UNO at a time. This means multiple files and Arduinos may be necessary to play a full MIDI file depending on the song. This transpiler will output the minimum number of code files in a .zip necessary to represent the given .mid.</p>
+<p>To sync the start of multiple Arduinos playback, it is recommended that a single button is wired to all Arduinos and programmed to start playback. Because of this, the outputted files will be txt files so the code can be easily copied rather than Arduino code files.</p>
 
 <FileDropzone accept=".mid" multiple={false} on:file={handleFile} name="files">
 	<svelte:fragment slot="meta">.mid allowed</svelte:fragment>
