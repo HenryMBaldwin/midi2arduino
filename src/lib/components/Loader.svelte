4d5317c2380728105148066e8aed0379c8524c8b
Henry Baldwin <henrymbaldwin@gmail.com>
2024-06-22 22:10:15 -0400

    Loader and loader button components.

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let size = '30px'; // Default size if not provided
+</script>
+
+<div class="flex justify-center items-center">
+    <div 
+        class="loader border-4 border-gray-200 border-t-primary-500 rounded-full animate-spin"
+        style="width: {size}; height: {size}; border-width: {parseInt(size) / 7}px;">
+    </div>
+</div>
+
+<style>
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+    .loader {
+        animation: spin 2s linear infinite;
+    }
+</style>
---
+++ src/lib/components/LoaderButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Loader from '$lib/components/Loader.svelte';
+    
+    export let loading: boolean = false;
+    export let btnClass: string = '';
+    export let btnId: string = '';
+    export let onClick = () => {};
+    export let text: string = '';
+    
+    </script>
+    
+    {#if !loading}
+        <button class={btnClass} on:click={onClick} id={btnId}>{text}</button>
+    {:else}
+        <Loader />
+    {/if}
+            
