8deb18355c7849087f7232be3df2e930d3c039cf
Henry Baldwin <henrymbaldwin@gmail.com>
2024-06-23 23:44:03 -0400

    Changed wording.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,7 +55,7 @@
 </script>
 
 <p>Welcome! MIDI2Arduino is a tool to convert MIDI files to be played on one of the most accessible Arduino platforms, the Arduino UNO. </p>
-<p>This transpiler converts midi to arduino code using the the Arduino Tone library. Due to the number of hardware timers on the Arduino UNO, only two tones can be generated on one Arduino UNO at a time. This means multiple files and Arduinos may be necessary to play a full MIDI file depending on the song. This transpiler will output the minimum number of code files in a .zip necessary to represent the given .mid.</p>
+<p>This transpiler converts midi to arduino code using the the Arduino Tone library. Due to the number of hardware timers on the Arduino UNO, only two tones can be generated on one Arduino UNO at a time. This means multiple files and Arduinos may be necessary to play a full MIDI file depending on the maximum number a simultaneous notes it contains. This transpiler will output the minimum number of code files in a .zip necessary to represent the given .mid.</p>
 <p>To sync the start of multiple Arduinos playback, it is recommended that a single button is wired to all Arduinos and programmed to start playback. Because of this, the outputted files will be txt files so the code can be easily copied rather than Arduino code files.</p>
 
 {#if uploadedFile}
